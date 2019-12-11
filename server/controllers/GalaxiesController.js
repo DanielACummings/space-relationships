fc9ede2672e7e951e72064f28f2fea82eaa6261c
Daniel <danielac@protonmail.com>
2019-12-11 14:47:36 -0700

    adds galaxies controller, service,  model w/ CRUD

---
+++ server/controllers/GalaxiesController.js
@@ -0,0 +1,55 @@
+import express from "express";
+import galaxiesService from "../services/GalaxiesService";
+
+export default class GalaxiesController {
+  constructor() {
+    this.router = express
+      .Router()
+      .get("", this.getAll)
+      .get('/:id', this.getById)
+      .post('', this.create)
+      .put('/:id', this.edit)
+      .delete('/:id', this.delete)
+  }
+
+  async getAll(req, res, next) {
+    try {
+      let data = await galaxiesService.getAll();
+      return res.send(data);
+    } catch (error) {
+      next(error);
+    }
+  }
+  async getById(req, res, next) {
+    try {
+      let data = await galaxiesService.getById(req.params.id)
+      return res.send(data)
+    } catch (error) {
+      next(error)
+    }
+  }
+  async create(req, res, next) {
+    try {
+      let data = await galaxiesService.create(req.body)
+      return res.status(201).send(data)
+    } catch (error) {
+      next(error)
+    }
+  }
+  async edit(req, res, next) {
+    try {
+      let data = await galaxiesService.edit(req.params.id, req.body)
+      return res.send(data)
+    } catch (error) {
+      next(error)
+    }
+  }
+  async delete(req, res, next) {
+    try {
+      await galaxiesService.delete(req.params.id)
+      return res.send('Successfully Deleted')
+    } catch (error) {
+      next(error)
+    }
+  }
+}

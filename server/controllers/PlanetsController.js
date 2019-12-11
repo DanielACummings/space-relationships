083f20ab21e963f6f0255128723a9cc5c2df1b92
Daniel <danielac@protonmail.com>
2019-12-11 15:54:40 -0700

    adds planet files with functioning CRUD

---
+++ server/controllers/PlanetsController.js
@@ -0,0 +1,55 @@
+import express from "express";
+import planetsService from "../services/PlanetsService";
+
+export default class PlanetsController {
+  constructor() {
+    this.router = express
+      .Router()
+      .get("", this.getAll)
+      .get('/:id', this.getById)
+      .post('', this.create)
+      .put('/:id', this.edit)
+      .delete('/:id', this.delete)
+  }
+
+  async getAll(req, res, next) {
+    try {
+      let data = await planetsService.getAll();
+      return res.send(data);
+    } catch (error) {
+      next(error);
+    }
+  }
+  async getById(req, res, next) {
+    try {
+      let data = await planetsService.getById(req.params.id)
+      return res.send(data)
+    } catch (error) {
+      next(error)
+    }
+  }
+  async create(req, res, next) {
+    try {
+      let data = await planetsService.create(req.body)
+      return res.status(201).send(data)
+    } catch (error) {
+      next(error)
+    }
+  }
+  async edit(req, res, next) {
+    try {
+      let data = await planetsService.edit(req.params.id, req.body)
+      return res.send(data)
+    } catch (error) {
+      next(error)
+    }
+  }
+  async delete(req, res, next) {
+    try {
+      await planetsService.delete(req.params.id)
+      return res.send('Successfully Deleted')
+    } catch (error) {
+      next(error)
+    }
+  }
+}
---
+++ server/models/Planet.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema
+const ObjectId = Schema.Types.ObjectId
+
+const Planet = new Schema(
+  {
+    name: { type: String, required: true },
+    kmWidth: { type: Number, required: true },
+    starId: { type: ObjectId, ref: 'star', required: true }
+  },
+  { timestamps: true, toJSON: { virtuals: true } }
+);
+
+export default Planet
